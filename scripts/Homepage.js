551fdf9c0427898821d923907e7ac1fe13a1508f
kenyosh <77033976+kenyosh@users.noreply.github.com>
2025-05-18 17:42:05 -0700

    added /a

--- scripts/Homepage.js
+++ scripts/Homepage.js
@@ -12,8 +12,9 @@ document.addEventListener("DOMContentLoaded", () => {
         albumDiv.className = "album-preview";
         albumDiv.innerHTML = `
           <a href="pages/Obon23Gallery.html?album=${encodeURIComponent(album.folder)}">
-          <h3>${album.title}</h3>
-          <img src="${album.folder}/${album.cover}" alt="${album.title}" />
+            <h3>${album.title}</h3>
+            <img src="${album.folder}/${album.cover}" alt="${album.title}" />
+          </a>
         `;
         container.appendChild(albumDiv);
       });
