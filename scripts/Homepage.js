11b148aedf3418ed8865d3462f4e274a0333a5fd
kenyosh <77033976+kenyosh@users.noreply.github.com>
2025-05-22 18:59:37 -0700

    Update Homepage.js

--- scripts/Homepage.js
+++ scripts/Homepage.js
@@ -11,7 +11,6 @@ document.addEventListener("DOMContentLoaded", () => {
         const albumDiv = document.createElement("div");
         albumDiv.className = "album-preview";
         albumDiv.innerHTML = `
-          <a href="pages/Obon23Gallery.html?album=${encodeURIComponent(album.folder)}">
             <h3>${album.title}</h3>
             <img src="`../${album.folder}/${album.cover}" alt="${album.title}" /`>
           </a>
